80f8b08feae1900dc0816de47601f99e39b44bc9
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-09-12 10:34:39 +0100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { Client } from "pg";
+import { getEnvVarOrFail } from "./support/envVarUtils";
+import { setupDBClientConfig } from "./support/setupDBClientConfig";
+
+dotenv.config();
+
+const dbClientConfig = setupDBClientConfig();
+const client = new Client(dbClientConfig);
+
+const app = express().use(express.json()).use(cors());
+
+app.get("/", async (_req, res) => {
+  res.json({ msg: "Hello! There's nothing interesting for GET /" });
+});
+
+app.get("/health-check", async (_req, res) => {
+  try {
+    await client.query("select now()");
+    res.status(200).send("system ok");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred. Check server logs.");
+  }
+});
+
+connectToDBAndStartListening();
+
+async function connectToDBAndStartListening() {
+  console.log("Attempting to connect to db");
+  await client.connect();
+  console.log("Connected to db!");
+
+  const port = getEnvVarOrFail("PORT");
+  app.listen(port, () => {
+    console.log(
+      `Server started listening for HTTP requests on port ${port}.  Let's go!`
+    );
+  });
+}
