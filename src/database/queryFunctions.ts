b6be8d26fd1fdb693edcea7541087528d29b7217
henrypgill <henrygill3@gmail.com>
2023-09-14 00:17:52 +0100

    isolated db queries into file

---
+++ src/database/queryFunctions.ts
@@ -0,0 +1,17 @@
+import dbClient from "./dbSetup";
+import { MinimalResource } from "./types";
+
+
+
+export async function getResources(): Promise<MinimalResource[]> {
+    const resources = await dbClient.fileQuery("select_resource", [1]).then(response => response.rows);
+    const tags = await dbClient.fileQuery("select_resource_tags").then(response => response.rows);
+
+    const resourcesWithTags = resources.map((resource) => {
+            return {...resource,
+                tag: tags.filter((t) => resource.id === t[0])
+            }
+    });
+
+    return resourcesWithTags
+}
--- src/routes/resources.ts
+++ src/routes/resources.ts
@@ -1,28 +1,13 @@
 import { Router } from "express";
 import { database } from "../server";
+import { getResources } from "../database/queryFunctions";
 
 const router = Router();
 
 router.get("/", async (_req, res) => {
   try {
-    const response =
-      await database.query(`SELECT resources.id, resources.title, resources.description, resources.created_at, authors.name as author_name
-    FROM resources
-    left join authors on authors.id=resources.author_id`);
-    const resources = response.rows;
-    const tags = (await database.query("SELECT name FROM tags")).rows;
-    const resourcesWithTags = resources.map((resource) => {
-      const retResource = {
-        ...resource,
-        tags: [
-          tags[Math.floor(Math.random() * tags.length)].name,
-          tags[Math.floor(Math.random() * tags.length)].name,
-          tags[Math.floor(Math.random() * tags.length)].name,
-        ],
-      };
-      return retResource;
-    });
-    res.status(200).json(resourcesWithTags);
+    const resources = getResources();
+    res.status(200).json(resources);
   } catch (error) {
     console.error(error);
     res.status(500).send("An error occurred. Check server logs.");
