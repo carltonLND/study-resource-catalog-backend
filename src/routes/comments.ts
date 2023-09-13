f58d0bc0562dabfb665ed329efd002024c5eed69
henrypgill <henrygill3@gmail.com>
2023-09-13 15:35:25 +0100

    added express router and starter routes

---
+++ src/routes/comments.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { listener } from "../server";
+
+const router = Router();
+
+router.get("/", async (_req, res) => {
+  try {
+    await listener.query("select now()");
+    res.status(200).send("system ok");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred. Check server logs.");
+  }
+});
+
+const commentsRouter = router;
+export default commentsRouter;
---
+++ src/routes/general.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { listener } from "../server";
+
+const router = Router();
+
+router.get("/", async (_req, res, next) => {
+  res.json({ msg: "Hello! There's nothing interesting for GET /" });
+  next()
+});
+
+router.get("/health-check", async (_req, res) => {
+  try {
+    await listener.query("select now()");
+    res.status(200).send("system ok");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred. Check server logs.");
+  }
+});
+
+
+const homeRouter = router;
+export default homeRouter;
