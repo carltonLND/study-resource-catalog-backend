77103ac94157a78571e1005f723cc7c1e8b0865f
henrypgill <henrygill3@gmail.com>
2023-09-14 11:22:20 +0100

    updated query to return correct columns

--- sql_queries/select_resource_tags.sql
+++ sql_queries/select_resource_tags.sql
@@ -1,4 +1,4 @@
-SELECT resource_tags.resource_id, resource_tags.tag_id FROM resources
+SELECT resource_tags.resource_id AS resource_id, tags.name AS tag_name FROM resources
 INNER JOIN
 resource_tags ON resource_tags.resource_id=resources.id
 LEFT JOIN
