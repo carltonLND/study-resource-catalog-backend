2bd860218ffa377c8dc5fac17035c47abb8f8a4b
henrypgill <henrygill3@gmail.com>
2023-09-19 17:51:31 +0100

    removed unused files

--- sql_queries/insert_recommendation.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO recommendations
-(resource_id, recommendation_type_id, content)
-VALUES
-($1, $2, $3);
-
-SELECT recommendations.content, recommendation_type.description FROM recommendations INNER JOIN recommendation_type ON recommendations.recommendation_type_id=recommendation_type.id
-WHERE recommendations.resource_id=$1;
--- sql_queries/insert_user_resource.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO user_resources (resource_id, user_id)
-VALUES
-($1, $2);
