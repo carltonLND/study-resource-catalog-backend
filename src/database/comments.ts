dabe890265df9fb1f57bbcaa1831f7cf1adfa84d
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-09-20 15:10:29 +0100

    Added query function for inserting a new comment

--- src/database/comments.ts
+++ src/database/comments.ts
@@ -1,3 +1,4 @@
+import { DbComment, NewComment } from "..";
 import { database } from "../server";
 
 export async function getResourceComments(resourceId: string) {
@@ -5,4 +6,17 @@ export async function getResourceComments(resourceId: string) {
     .query("SELECT * FROM comments WHERE resource_id= $1", [resourceId])
     .then((response) => response.rows);
   return comments;
+}
+
+export async function insertResourceComment(
+  comment: NewComment
+): Promise<DbComment> {
+  const newComment = await database
+    .query(
+      "INSERT INTO comments (user_id, resource_id, content) VALUES ($1, $2, $3) RETURNING *",
+      [comment.user_id, comment.resource_id, comment.content]
+    )
+    .then((response) => response.rows[0]);
+
+  return newComment;
 }
