e71a8164c6cd37d5694ab3c9a439c251c83d1006
henrypgill <henrygill3@gmail.com>
2023-09-20 18:06:46 +0100

    altered comment routes to return correct type

--- src/database/comments.ts
+++ src/database/comments.ts
@@ -1,39 +1,42 @@
-import { DbComment, NewComment } from "..";
+import { DbCommentWithUser, NewComment } from "..";
 import { database } from "../server";
 
-
-function DbComment_to_Comment({id, resource_id, user_id, user_name, user_is_faculty, content, created_at}: DbComment) {
+function DbComment_to_Comment({
+  id,
+  resource_id,
+  user_id,
+  user_name,
+  user_is_faculty,
+  content,
+  created_at,
+}: DbCommentWithUser) {
   return {
     id,
     resource_id,
     user: {
       id: user_id,
       name: user_name,
-      is_faculty: user_is_faculty
+      is_faculty: user_is_faculty,
     },
     content,
     created_at,
   };
 }
 
-
-
-
 export async function getResourceComments(resourceId: string) {
   const comments = await database
-    .fileQuery<DbComment>("select_resource_comments", [resourceId])
+    .fileQuery<DbCommentWithUser>("select_resource_comments", [resourceId])
     .then((response) => response.rows);
   return comments.map(DbComment_to_Comment);
 }
 
-export async function insertResourceComment(
-  comment: NewComment
-): Promise<DbComment> {
+export async function insertResourceComment({
+  user_id,
+  resource_id,
+  content,
+}: NewComment): Promise<DbCommentWithUser> {
   const newComment = await database
-    .query(
-      "INSERT INTO comments (user_id, resource_id, content) VALUES ($1, $2, $3) RETURNING *",
-      [comment.user_id, comment.resource_id, comment.content]
-    )
+    .fileQuery("insert_comment", [user_id, resource_id, content])
     .then((response) => response.rows[0]);
 
   return newComment;
