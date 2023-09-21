ff10c1de21ad50d7dead3330351118ae7054a1d7
henrypgill <henrygill3@gmail.com>
2023-09-21 09:24:39 +0100

    added error handling and route for like toggling

--- src/routes/resources.ts
+++ src/routes/resources.ts
@@ -8,6 +8,7 @@ import {
   getResourceLikeCount,
   getResourceLikeCountAndIfLiked,
   getResourceLikes,
+  updateResourceLike,
 } from "../database/likes";
 import {
   getMinimalResources,
@@ -51,48 +52,85 @@ router.post<Record<string, never>, FullResource, NewResource>(
 );
 
 router.get<{ resourceId: string }>("/:resourceId/likes", async (_req, res) => {
-  const { resourceId } = _req.params;
-  const likes = await getResourceLikes(parseInt(resourceId));
-  res.status(200).json(likes);
+  try {
+    const { resourceId } = _req.params;
+    const likes = await getResourceLikes(parseInt(resourceId));
+    res.status(200).json(likes);
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("An error occurred. Check server logs.")
+  }
 });
 
 router.get<{ resourceId: string }>(
   "/:resourceId/comments",
   async (req, res) => {
-    const { resourceId } = req.params;
-    const comments = await getResourceComments(resourceId);
-    res.status(200).json(comments);
-  }
+    try {
+      const { resourceId } = req.params;
+      const comments = await getResourceComments(resourceId);
+      res.status(200).json(comments);
+    } catch (error) {
+      console.error(error)
+      res.status(500).send("An error occurred. Check server logs.")
+    }
+    }
 );
 
-router.post<Record<string, never>, DbComment, NewComment>(
+router.post<Record<string, never>, DbComment | string, NewComment>(
   "/:resourceId/comments",
   async (req, res) => {
-    const newComment = await insertResourceComment(req.body);
-    res.status(201).json(newComment);
-  }
+    try {
+      const newComment = await insertResourceComment(req.body);
+      res.status(201).json(newComment);
+    } catch (error) {
+      console.error(error)
+      res.status(500).send("An error occurred. Check server logs.")
+    }
+    }
 );
 
 router.get<{ resourceId: string }>(
   "/:resourceId/likes/count",
   async (req, res) => {
-    const { resourceId } = req.params;
-    const likes = await getResourceLikeCount(parseInt(resourceId));
-    res.status(200).json(likes);
+    try {
+      const { resourceId } = req.params;
+      const likes = await getResourceLikeCount(parseInt(resourceId));
+      res.status(200).json(likes);
+    } catch (error) {
+      console.error(error)
+      res.status(500).send("An error occurred. Check server logs.")
+    }
   }
 );
 
 router.get<{ resourceId: string; userId: string }>(
   "/:resourceId/likes/count/:userId",
   async (req, res) => {
-    const { resourceId, userId } = req.params;
-    const likesWithUser = await getResourceLikeCountAndIfLiked(
-      parseInt(resourceId),
-      parseInt(userId)
-    );
-    res.status(200).json(likesWithUser);
+    try {
+      const { resourceId, userId } = req.params;
+      const likesWithUser = await getResourceLikeCountAndIfLiked(
+        parseInt(resourceId),
+        parseInt(userId)
+        );
+        res.status(200).json(likesWithUser);
+      } catch (error) {
+        console.error(error)
+        res.status(500).send("An error occurred. Check server logs.")
+      }
   }
 );
+
+router.patch<{ resourceId: string; userId: string }>("/:resource_id/likes/:user_id", async (req, res) => {
+  try {
+
+    const { resourceId, userId } = req.params;
+    const result = await updateResourceLike(parseInt(resourceId), parseInt(userId));
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("An error occurred. Check server logs.")
+  }
+});
 
 const resourcesRouter = router;
 export default resourcesRouter;
